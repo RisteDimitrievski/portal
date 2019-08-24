e5e682974ca7279d387d8cb4aac1e3c2c29bf25e
DESKTOP-CF8UL3V\User1 <ristedimitrievski23@gmail.com>
2019-08-25 00:20:00 +0200

    Added controller for headers, requests, cookie.

---
+++ controllers/Header.class.php
@@ -0,0 +1,100 @@
+<?php 
+/* Controller for Headers
+  @author Riste Dimitrievski
+  @version 1.0.0
+ */
+namespace Portal\Headers;
+session_start();
+ob_start();
+class Request
+{
+var $get = array();
+var $post = array();
+public function __construct($type,$data){
+$this->data = $data;
+$this->type = $type;
+if(!isset($this->type)){
+trigger_error("The request type can't be empty");
+}
+
+switch($this->type){
+case "get":
+     return self::get();
+	 exit;
+	 break;
+case "post":
+    return self::request();
+	break;
+case "cookie":
+    return self::request();
+	break;
+
+}
+}
+public function get(){
+global $data;
+return $_GET["{$this->data}"];
+}
+public function request(){
+switch($this->type){
+case "post":
+return $_POST["{$this->data}"];
+break;
+case "cookie":
+return $_COOKIE["{$this->data}"];
+break;
+}
+}
+}
+
+class Cookie
+{
+/*
+@param $options = SAVE : READ
+@param $parameters = array cookie elements
+~ name
+~ value
+~ expires
+~ path (optional)
+~ domain
+~ protocol (HTTP / HTTPS)
+~ httponly (False ? True)
+*/
+
+public function __construct($parameters){
+if(!isset($parameters)){
+trigger_error("Fatal error: the cookie parameters are required in order to create cookie"); 
+}
+if(is_array($parameters)){
+foreach($parameters as $this->key => $this->value){
+$this->cookieName = $parameters['name'];
+$this->cookieValue = $parameters['value'];
+$this->cookieExpires = $parameters['expires'];
+$this->cookiePath = $parameters['path'];
+$this->cookieDomain = $parameters['domain'];
+$this->cookieSecure = $parameters['protocol'];
+$this->cookieHttpOnly = $parameters['httponly'];
+if(!isset($this->cookiePath)){
+$this->cookiePath = "./";
+}
+if(!isset($this->cookieSecure)){
+$this->cookieSecure = false;
+}
+if(!isset($this->cookieHttpOnly)){
+$this->cookieHttpOnly = false;
+}
+}
+} else{
+trigger_error("Fatal Error: The parameters should be passed in array in order to create cookie");
+}
+self::save();
+
+
+}
+public function save(){
+if(!isset($this->cookieDomain)){
+setcookie($this->cookieName,$this->cookieValue,$this->cookieExpires,$this->cookiePath,getenv("HTTP_HOST"),$this->cookieSecure,$this->cookieHttpOnly);
+}
+setcookie($this->cookieName,$this->cookieValue,$this->cookieExpires,$this->cookiePath,$this->cookieDomain,$this->cookieSecure,$this->cookieHttpOnly);
+}
+}
