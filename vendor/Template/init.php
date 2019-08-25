64ac1fefb93fc9d74c543bac6a1ca7de275ba551
DESKTOP-CF8UL3V\User1 <ristedimitrievski23@gmail.com>
2019-08-25 21:02:08 +0200

    Updated the template loader.

--- vendor/Template/init.php
+++ vendor/Template/init.php
@@ -1,6 +1,9 @@
 <?php 
 include("./Smarty.class.php");
+include("./controllers/Header.class.php");
 $template = new Smarty();
 $template->setTemplateDir("./views/");
 $template->setCompileDir("./views_c/");
 $template->setConfigDir("./configs/");
+$sesija = new Session(NULL,NULL);
+$sesija->start();
